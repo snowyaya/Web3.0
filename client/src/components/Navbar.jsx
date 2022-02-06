602c8f8f12f0c999594ead52a310ebaff7d8f53e
snowyaya <yayingliang@gmail.com>
2022-02-06 00:58:50 -0500

    update UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,10 +17,10 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+        {["Market", "Tutorials"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+        <li className="bg-[#24A19C] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#96CEB4]">
           Login
         </li>
       </ul>
@@ -37,7 +37,7 @@ const Navbar = () => {
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
+            {["Market", "Tutorials"].map(
               (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
             )}
           </ul>
